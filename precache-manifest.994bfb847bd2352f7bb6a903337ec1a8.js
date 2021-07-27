8c63613eb77f9a55c1f585e743b4546e481e604e
Tetiana Yasko <taniayasko2015@gmail.com>
2021-07-27 15:24:20 +0300

    Updates

---
+++ precache-manifest.994bfb847bd2352f7bb6a903337ec1a8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5412b7dc2e0f10db0b79114bc4f9d13",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3ae271236ab3fc024c84",
+    "url": "./static/css/2.a1df12c0.chunk.css"
+  },
+  {
+    "revision": "9dcc623c03070c0925b4",
+    "url": "./static/css/main.789427f3.chunk.css"
+  },
+  {
+    "revision": "3ae271236ab3fc024c84",
+    "url": "./static/js/2.bcbb2c34.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.bcbb2c34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dcc623c03070c0925b4",
+    "url": "./static/js/main.fb8baea8.chunk.js"
+  },
+  {
+    "revision": "79d17474afd20bce64b6",
+    "url": "./static/js/runtime-main.12497659.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.520d4294590a353bae9dfdf7775894f7.js"
+  "./precache-manifest.994bfb847bd2352f7bb6a903337ec1a8.js"
 );
 
 self.addEventListener('message', (event) => {
